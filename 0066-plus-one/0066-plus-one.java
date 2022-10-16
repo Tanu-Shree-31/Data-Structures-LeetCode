834914c02fc8174d97fcc4663dec01c29ba162a5
Tanushree B S <69342524+Tanu-Shree-31@users.noreply.github.com>
2022-10-16 07:39:27 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (23.04%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+         int index;
+        for(index = digits.length - 1; index >= 0 &&digits[index] == 9; index--) {    
+            digits[index] = 0;
+        }
+        if (index == -1) {  // edge case for all digits are 9, so becomes 1 with n zeroes
+            int[] newDigits = new int[digits.length + 1];
+            newDigits[0] = 1;   // new int[] initialise with zeroes, so set first digit as 1
+            return newDigits;
+        }
+        digits[index]++;    // found the non 9 digit, just add 1
+        return digits;
+    }
+}
