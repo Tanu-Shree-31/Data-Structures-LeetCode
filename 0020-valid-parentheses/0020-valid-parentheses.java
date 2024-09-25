f83b7dd7e2bc1b98d3198d244fb08a0cd0948f48
Tanushree B S <69342524+Tanu-Shree-31@users.noreply.github.com>
2024-09-25 13:48:50 +0530

    Time: 1 ms (98.58%), Space: 41.5 MB (36.52%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String a) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : a.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
