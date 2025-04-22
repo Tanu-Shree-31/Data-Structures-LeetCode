5ae748fabd8df2e7b4cd2fcb2737300ab3905820
Tanushree B S <69342524+Tanu-Shree-31@users.noreply.github.com>
2025-04-22 22:52:36 +0530

    Time: 27 ms (5.5%), Space: 45 MB (18.29%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // O(s+t)
+        // HashSet<Character> h = new HashSet<Character>();
+        // for(int i=0; i<s.length(); i++){
+        //     char c = s.charAt(i);
+        //     h.add(c);
+        // }
+        // for(int i=0; i<t.length(); i++){
+        //     char c = t.charAt(i);
+        //     if(!h.contains(c)){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        HashMap<Character, Integer> countS = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> countT = new HashMap<Character, Integer>();
+        for(int i=0; i<s.length(); i++) {
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i),0)+1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i),0)+1);
+        }
+        return countS.equals(countT);
+
+        //TC: O(n)
+        //SC: O(1) - operations of hashmap takes O(1)
+    }
+}
