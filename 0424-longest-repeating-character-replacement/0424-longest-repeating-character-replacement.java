c5ec041d0119a175ea379248b5395574fc4321ea
Tanushree B S <69342524+Tanu-Shree-31@users.noreply.github.com>
2025-08-16 11:24:45 +0530

    Time: 10 ms (67.24%), Space: 44.6 MB (83.1%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int l=0,r=0,maxlen=0, maxfreq=0;
+        int n=s.length();
+        int hash[] = new int[26];
+        while(r<n){
+            hash[s.charAt(r)-'A']++;
+            maxfreq= Math.max(maxfreq,hash[s.charAt(r)-'A']);
+            if(r-l+1 - maxfreq>k){
+                hash[s.charAt(l)-'A']--;
+                l++;
+            }
+            maxlen=Math.max(maxlen,r-l+1);
+            r++;
+        }
+        return maxlen;
+    }
+}
