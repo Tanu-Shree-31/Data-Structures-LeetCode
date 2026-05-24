a4986f72bc8e174e6da1b0a33e1859626edc9168
Tanushree B S <69342524+Tanu-Shree-31@users.noreply.github.com>
2026-05-24 20:39:28 +0530

    Time: 6 ms (45.12%), Space: 44.2 MB (89.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,44 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // O(s+t)
-        // HashSet<Character> h = new HashSet<Character>();
-        // for(int i=0; i<s.length(); i++){
-        //     char c = s.charAt(i);
-        //     h.add(c);
-        // }
-        // for(int i=0; i<t.length(); i++){
-        //     char c = t.charAt(i);
-        //     if(!h.contains(c)){
-        //         return false;
-        //     }
-        // }
-        // return true;
 
-        if(s.length()!=t.length()){
+        // Step 1: Length check
+        if (s.length() != t.length()) {
             return false;
         }
 
-        // HashMap<Character, Integer> countS = new HashMap<Character, Integer>();
-        // HashMap<Character, Integer> countT = new HashMap<Character, Integer>();
-        // for(int i=0; i<s.length(); i++) {
-        //     countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i),0)+1);
-        //     countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i),0)+1);
-        // }
-        // return countS.equals(countT);
+        // Step 2: Frequency array
+        int[] arr = new int[26];
 
-        //TC: O(n)
-        //SC: O(1) - operations of hashmap takes O(1)
-
-        int[] count = new int[26];
-        for(int i=0; i<s.length(); i++) {
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
+        // Step 3: Increment for s, decrement for t
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
-        for(int val: count){
-            if(val!=0){
+
+        // Step 4: Check if all frequencies are zero
+        for (int count : arr) {
+            if (count != 0) {
                 return false;
             }
         }
+
         return true;
     }
 }
