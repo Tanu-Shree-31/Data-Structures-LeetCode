95c33d9f75ec13b615e3881e4b662fc31b835e2a
Tanushree B S <69342524+Tanu-Shree-31@users.noreply.github.com>
2026-05-29 19:40:19 +0530

    Time: 11 ms (96.38%), Space: 47.4 MB (89.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,24 +1,63 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // brute force approach - using sorting
+        // // brute force approach - using sorting
+        // // tc: O (n x mlogm) sc: O(n)
+        // HashMap<Integer,Integer> mpp = new HashMap<>();
+        // // step1: build the number : freq hashmap
+        // for(int i: nums){
+        //     mpp.put(i, mpp.getOrDefault(i, 0)+1);
+        // }
+        // // step2: sort this hashmap based on frequencies
+        // // for sorting operation, we need to add these (k,v) pair in list
+        // // in list we will have as (v,k) - as we need to sort by frequencies
+        // List <int[]> list = new ArrayList<>();
+        // for(Map.Entry<Integer, Integer> entry: mpp.entrySet()){
+        //     list.add(new int[] {entry.getValue(), entry.getKey()});
+        // }
+        // // step 3: sort this list based on frequencies descending order
+        // list.sort((a,b)->b[0]-a[0]); 
+        // int res[] = new int[k];
+        // for(int i=0; i<k; i++){
+        //     res[i] = list.get(i)[1]; // get the 2nd ele, which is original value/ number
+        // }
+        // return res;
+
+        // optimised approach using bucket sort
         HashMap<Integer,Integer> mpp = new HashMap<>();
         // step1: build the number : freq hashmap
         for(int i: nums){
             mpp.put(i, mpp.getOrDefault(i, 0)+1);
         }
-        // step2: sort this hashmap based on frequencies
-        // for sorting operation, we need to add these (k,v) pair in list
-        // in list we will have as (v,k) - as we need to sort by frequencies
-        List <int[]> list = new ArrayList<>();
-        for(Map.Entry<Integer, Integer> entry: mpp.entrySet()){
-            list.add(new int[] {entry.getValue(), entry.getKey()});
+        //step2: sort this hashmap based on freq
+        List<Integer>[] list = new ArrayList[nums.length+1];
+        for(Map.Entry<Integer,Integer> entry: mpp.entrySet()){
+            int number = entry.getKey();
+            int value = entry.getValue();
+            // create the bucket only if it does not exist
+            if(list[value]==null){
+                list[value] = new ArrayList<>();
+            }
+            list[value].add(number);
         }
-        // step 3: sort this list based on frequencies descending order
-        list.sort((a,b)->b[0]-a[0]); 
+        //step 3: only return from last 'k' ele from list
         int res[] = new int[k];
-        for(int i=0; i<k; i++){
-            res[i] = list.get(i)[1]; // get the 2nd ele, which is original value/ number
+        int index=0;
+        for(int i=list.length-1; i>0; i--){
+        
+            if(list[i]!=null){
+                for(int num: list[i]){
+                    res[index]=num;
+                    index++;
+                    if(index==k){
+                        return res;
+                    }
+
+                }
+            }
         }
         return res;
+
+
+
     }
 }
