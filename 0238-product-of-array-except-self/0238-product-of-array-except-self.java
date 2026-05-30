6b4d43942dc89e32e04076318c710f236cf5ec98
Tanushree B S <69342524+Tanu-Shree-31@users.noreply.github.com>
2026-05-30 15:08:11 +0530

    Time: 2 ms (95.2%), Space: 64 MB (94.67%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,19 +1,27 @@
 class Solution {
-    public int[] productExceptSelf(int[] arr) {
-        int n = arr.length;
-        int res[] = new int[n];
-        
-        res[0] = 1;
-        for(int i=1; i<n; i++){
-            res[i] = res[i-1] * arr[i-1]; // calculating the prefix in the res array only
+    public int[] productExceptSelf(int[] nums) {
+        // compute the left side product array
+        int leftArr[] = new int[nums.length];
+        int leftProd = 1;
+        leftArr[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            leftProd = leftProd * nums[i-1];
+            leftArr[i] = leftProd;
         }
 
-        // we dont want the last pos bcz already it would be same as prefixsum value
-        int suffix = 1;
-        for(int i=n-2; i>=0; i--){
-            suffix = suffix * arr[i+1];
-            res[i] = suffix * res[i];
+        // using leftArr and original array, compute the ans
+        int ans[] = new int[nums.length];
+        int rightProd = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i == nums.length - 1) {
+                rightProd = 1;
+            } else {
+                rightProd = rightProd * nums[i + 1];
+            }
+            ans[i] = rightProd * leftArr[i];
+
         }
-        return res;
+        return ans;
+
     }
 }
