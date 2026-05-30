50197c9b3c4298c7c8be8cb6aaf85ba31b70f313
Tanushree B S <69342524+Tanu-Shree-31@users.noreply.github.com>
2026-05-30 16:01:43 +0530

    Time: 335 ms (3.62%), Space: 73.8 MB (6.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,16 +1,36 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // compute the left side product array
-        int leftArr[] = new int[nums.length];
+        // // compute the left side product array
+        // int leftArr[] = new int[nums.length];
+        // int leftProd = 1;
+        // leftArr[0] = 1;
+        // for (int i = 1; i < nums.length; i++) {
+        //     leftProd = leftProd * nums[i-1];
+        //     leftArr[i] = leftProd;
+        // }
+
+        // // using leftArr and original array, compute the ans
+        // int ans[] = new int[nums.length];
+        // int rightProd = 1;
+        // for (int i = nums.length - 1; i >= 0; i--) {
+        //     if (i == nums.length - 1) {
+        //         rightProd = 1;
+        //     } else {
+        //         rightProd = rightProd * nums[i + 1];
+        //     }
+        //     ans[i] = rightProd * leftArr[i];
+        // }
+        // return ans;
+
+        //Optimised - instead of having seperate array for leftArr - reuse the same ans array only
+        int ans[] = new int[nums.length];
         int leftProd = 1;
-        leftArr[0] = 1;
+        ans[0] = 1;
         for (int i = 1; i < nums.length; i++) {
             leftProd = leftProd * nums[i-1];
-            leftArr[i] = leftProd;
+            ans[i] = leftProd;
         }
 
-        // using leftArr and original array, compute the ans
-        int ans[] = new int[nums.length];
         int rightProd = 1;
         for (int i = nums.length - 1; i >= 0; i--) {
             if (i == nums.length - 1) {
@@ -18,10 +38,10 @@ class Solution {
             } else {
                 rightProd = rightProd * nums[i + 1];
             }
-            ans[i] = rightProd * leftArr[i];
+            ans[i] = rightProd * ans[i];
+            System.out.println(ans[i]);
 
         }
         return ans;
-
     }
 }
