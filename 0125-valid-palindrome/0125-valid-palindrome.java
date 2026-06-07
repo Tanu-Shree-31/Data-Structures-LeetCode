994c715f774b8d4227b4d38a2d296c529bc788c5
Tanushree B S <69342524+Tanu-Shree-31@users.noreply.github.com>
2026-06-07 22:06:42 +0530

    Time: 2 ms (99.16%), Space: 44.3 MB (65.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,33 +1,33 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // StringBuilder newStr = new StringBuilder();
-        // for (char c : s.toCharArray()) {
-        //     if (Character.isLetterOrDigit(c)) {
-        //         newStr.append(Character.toLowerCase(c));
-        //     }
-        // }
-        // return newStr.toString().equals(newStr.reverse().toString());
-
-        int l=0,r=s.length()-1;
-        while(l<r){
-            while(l<r && !isAlphaNum(s.charAt(l))){
+        // Two pointers approach in one traversal without using another string
+        int l = 0, r = s.length()-1;
+        while(l<r){ // because the middle character is already equal to l and r, so you don't need l<=r
+            // move until l is alphanumeric
+            while(l < r && !alphaNumeric(s.charAt(l))) {
                 l++;
             }
-            while(r>l && !isAlphaNum(s.charAt(r))){
+
+            // move until r is alphanumeric
+            while(r > l && !alphaNumeric(s.charAt(r))) {
                 r--;
             }
-            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
+
+            // compare lowercase versions
+            if(Character.toLowerCase(s.charAt(l)) == Character.toLowerCase(s.charAt(r))) {
+                l ++;
+                r--;
+            } else { // if both characters are not equal, then return false here only.
                 return false;
             }
-            l++;
-            r--;
+
         }
         return true;
     }
-    public boolean isAlphaNum(char c){
-        if((c >= 'A' && c<='Z') || (c >= 'a' && c<='z') || (c >= '0' && c<='9')){
-            return true;
-        }
+ private static boolean alphaNumeric(char c) {
+        if(c >='A' && c<='Z' || c>='a' && c<='z' || c>='0' && c<='9') return true;
+        // can also use - Character.isLetterOrDigit(c);
         return false;
     }
+
 }
